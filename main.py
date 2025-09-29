c63dbb02be5b75e28f2a7f4054cabc2a057cdcdb
Aurélien <aurelien.mopty@edu.esiee.fr>
2025-09-29 13:05:46 +0000

    07-asciiart terminé

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#### Imports et définition des variables globales
-
-# Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
 
-
-#### Fonctions secondaires
-
-
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères
+      passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +11,26 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    l=[]
+    lst_caractere=[s[0]]
+    lst_occurences=[1]
+    i=1
+    while i<len(s):
+        if s[i]==s[i-1]:
+            lst_occurences[-1]+=1
+            i+=1
+        else:
+            lst_caractere.append(s[i])
+            lst_occurences.append(1)
+            i+=1
+    for elt in zip(lst_caractere,lst_occurences):
+        l.append(elt)
+    return l
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères
+     passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,18 +38,14 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
-
-#### Fonction principale
-
+    if len(s)==0:
+        return []
+    reste = artcode_r(s[1:])
+    if reste and reste[0][0] == s[0]:
+        caractere, occ = reste[0]
+        reste[0] = (caractere, occ + 1)
+        return reste
+    return [(s[0], 1)] + reste
 
 def main():
     print(artcode_i('MMMMaaacXolloMM'))
